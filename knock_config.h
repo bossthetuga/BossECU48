396f2a5e7fc94ba7d492f95bfa41d350db4230a8
BossTheTuga <theboss720p@gmail.com>
2024-11-06 18:15:51 +0000

    Update knock_config.h

--- knock_config.h
+++ knock_config.h
@@ -7,9 +7,14 @@
 // Knock is on ADC3
 #define KNOCK_ADC ADCD3
 
-// knock 1 - pin PA3
-#define KNOCK_ADC_CH1 ADC_CHANNEL_IN3
-#define KNOCK_PIN_CH1 Gpio::A3
+// knock 1 - pin PC0
+#define KNOCK_ADC_CH1 ADC_CHANNEL_IN10
+#define KNOCK_PIN_CH1 Gpio::C0
+
+// knock 2 - pin PC1
+#define KNOCK_HAS_CH2 true
+#define KNOCK_ADC_CH2 ADC_CHANNEL_IN11
+#define KNOCK_PIN_CH2 Gpio::C1
 
 // Sample rate & time - depends on the exact MCU
 #define KNOCK_SAMPLE_TIME ADC_SAMPLE_84
